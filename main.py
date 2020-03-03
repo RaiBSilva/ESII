15617f56d02d6618400e59ec05912402ed0384b4
lCamiloR <61637025+lCamiloR@users.noreply.github.com>
2020-03-03 14:56:28 -0300

    Agora o algoritmo pega o texto em string

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import requests
-import urllib.request
-import json
+import lxml
+from bs4 import BeautifulSoup
 
-res = urllib.request.urlopen('http://www.licitacao.pmmc.com.br/Transparencia/vencimentos2')
-res_body = res.read()
+source = requests.get('http://www.licitacao.pmmc.com.br/Transparencia/vencimentos2').text
+soup = BeautifulSoup(source, 'lxml')
 
-print(res_body)
+# kill all script and style elements
+for script in soup(["script", "style"]):
+    script.extract()    # rip it out
+
+# get text
+text = soup.get_text()
+
+# break into lines and remove leading and trailing space on each
+lines = (line.strip() for line in text.splitlines())
+# break multi-headlines into a line each
+chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+# drop blank lines
+text = '\n'.join(chunk for chunk in chunks if chunk)
+
+# o texto da pagina agora ta em string
+print(text)
