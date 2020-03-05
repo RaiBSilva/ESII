c2baed9a982bb6439e379f3ce8fbeeb13a42a4a5
lCamiloR <61637025+lCamiloR@users.noreply.github.com>
2020-03-05 11:34:37 -0300

    adicionando o script do banco de dados para o sprint 0

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import sitePMC
+import funcoesBD
+
+objDB = funcoesBD.dataBase("127.0.0.1", "sprint_user", "123456", "bancosprint0")
 
 url = 'http://www.licitacao.pmmc.com.br/Transparencia/vencimentos2'
 
 objMogi = sitePMC.siteMogi(url)
 objMogi.getTexto()
 
-caracteres = ['{','[',']','"','servidores',',',':rgf:','rgf:','nome','cargo','rendimentos']
+caracteres = ['{','[',']','"','servidores',',',':rgf:','rgf:','nome','cargo','rendimentos','.']
 
 text = objMogi.filtraDados(caracteres)
 
-for i in text:
-    print(i + "\n")
+for i in range(0, len(text)):
+    linha = text[i].split(":")
+    if len(linha) == 4:
+        servidorPublico = (linha[0], linha[1], linha[2], linha[3])
+        objDB.insertInTB(servidorPublico)
