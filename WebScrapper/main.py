3e1824f9a9abebd127525c4fe4cf40b23051764f
lCamiloR <61637025+lCamiloR@users.noreply.github.com>
2020-03-11 10:13:29 -0300

    Update main.py

--- WebScrapper/main.py
+++ WebScrapper/main.py
@@ -1,7 +1,7 @@
 import sitePMC
 import funcoesBD
 
-objDB = funcoesBD.dataBase("127.0.0.1", "sprint_user", "123456", "bancosprint0")
+objDB = funcoesBD.dataBase("DSN=abc;Server=localhost\sqlexpress;Database=sprint0;Trusted_connection=yes;")
 
 url = 'http://www.licitacao.pmmc.com.br/Transparencia/vencimentos2'
 
@@ -16,6 +16,8 @@ servidorPublico = []
 for i in range(0, len(text)):
     linha = text[i].split(":")
     if len(linha) == 4:
-        servidorPublico.append((linha[0], linha[1], linha[2], linha[3]))
+        salario = float(linha[3])/100
+        salario = "%.2f" %salario
+        servidorPublico.append((linha[0], linha[1], linha[2], salario))
 
 objDB.insertInTB(servidorPublico)
