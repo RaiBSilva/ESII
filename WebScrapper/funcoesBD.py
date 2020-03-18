a78b8d45c5ab4ba6a84e6f716403927bc9a7e4e6
lCamiloR <61637025+lCamiloR@users.noreply.github.com>
2020-03-18 14:20:27 -0300

    Update funcoesBD.py

--- WebScrapper/funcoesBD.py
+++ WebScrapper/funcoesBD.py
@@ -5,6 +5,11 @@ class dataBase:
         self.sqldb = pyodbc.connect(conectString)
         self.sqlCursor = self.sqldb.cursor()
     def insertInTB(self, servidorPublico):
+        try:
+            self.sqlCursor.execute("TRUNCATE TABLE servidores;")
+        except:
+            print("Tabela ja vazia")
+            
         self.sqlComando = 'INSERT INTO servidores(rgf, nome_servidor, cargo_servidor, salario_servidor) VALUES (?, ?, ?, ?);'
         self.sqlCursor.executemany(self.sqlComando, servidorPublico)
         self.sqldb.commit()
