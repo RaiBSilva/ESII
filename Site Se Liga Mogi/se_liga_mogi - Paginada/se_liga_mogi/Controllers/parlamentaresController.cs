2d12bc7b27e85f71e716f05d0f16cc8b41b52cc1
Rai Benjamin da Silva <rai.benjamindasilva@gmail.com>
2020-05-31 15:37:23 -0300

    Adicionada pagina de visualizacao de sparlamentares e sua assiduidade

---
+++ Site Se Liga Mogi/se_liga_mogi - Paginada/se_liga_mogi/Controllers/parlamentaresController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using se_liga_mogi.Models;
+
+namespace se_liga_mogi.Controllers
+{
+    public class parlamentaresController : Controller
+    {
+        private Se_Liga_MogiEntities1 db = new Se_Liga_MogiEntities1();
+
+        // GET: parlamentares
+        public ActionResult Index(int pagina = 1, string Pesquisa = "")
+        {
+            var q = db.parlamentares.AsQueryable();
+            if (!string.IsNullOrEmpty(Pesquisa))
+            {
+                q = q.Where(c => c.nome_parlamentar.Contains(Pesquisa));
+            }
+            q = q.OrderBy(c => c.nome_parlamentar);
+            ViewBag.CurrentSort = Pesquisa;
+            return View(q.ToPagedList(pagina, 10));
+        }
+
+        public ActionResult Detalhes(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            parlamentares parlamentares = db.parlamentares.Find(id);
+            if (parlamentares == null)
+            {
+                return HttpNotFound();
+            }
+            presenca_parlamentares presenca = db.presenca_parlamentares.Find(id);
+            ViewBag.nomeParlamentar = parlamentares.nome_parlamentar;
+            return View(presenca);
+        }
+    }
+}
