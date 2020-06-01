9d731d4bf64e0d4aa2686a709f1f359e769cb9e7
Rai Benjamin da Silva <rai.benjamindasilva@gmail.com>
2020-06-01 18:22:59 -0300

    Adicao de filtros para pagina de servidores

--- Site Se Liga Mogi/se_liga_mogi - 1.0/se_liga_mogi/Controllers/servidoresController.cs
+++ Site Se Liga Mogi/se_liga_mogi - 1.0/se_liga_mogi/Controllers/servidoresController.cs
@@ -14,15 +14,59 @@ namespace se_liga_mogi.Controllers
     public class servidoresController : Controller
     {
         private Se_Liga_MogiEntities1 db = new Se_Liga_MogiEntities1();
-        public ActionResult Index(int pagina = 1, string Pesquisa = "")
+        public ActionResult Index(
+            int pagina = 1,
+            string Pesquisa = "",
+            string Filtro = "",
+            int? min = 0,
+            int? max = 100000)
         {
             var q = db.servidores.AsQueryable();
             if (!string.IsNullOrEmpty(Pesquisa))
             {
                 q = q.Where(c => c.nome_servidor.Contains(Pesquisa));
             }
-            q = q.OrderBy(c => c.nome_servidor);
+            if (!string.IsNullOrEmpty(Filtro))
+            {
+                switch (Filtro)
+                {
+                    case "s":
+                        if (min != null) {
+                            q = q.Where(c => c.salario_servidor > min);
+                        }
+                        if (max != null) {
+                            q = q.Where(c => c.salario_servidor < max);
+                        }
+                        break;
+                    case "d":
+                        if (min != null)
+                        {
+                            q = q.Where(c => c.descontos_servidor > min);
+                        }
+                        if (max != null)
+                        {
+                            q = q.Where(c => c.descontos_servidor < max);
+                        }
+                        break;
+                    case "r":
+                        if (min != null)
+                        {
+                            q = q.Where(c => c.salario_liquido_servidor > min);
+                        }
+                        if (max != null)
+                        {
+                            q = q.Where(c => c.salario_liquido_servidor < max);
+                        }
+                        break;
+                    default:
+                        return HttpNotFound();
+                }
+            }
+            q = q.OrderBy(c => c.salario_liquido_servidor);
             ViewBag.CurrentSort = Pesquisa;
+            ViewBag.FiltroAtual = Filtro;
+            ViewBag.Min = min;
+            ViewBag.Max = max;
             return View(q.ToPagedList(pagina, 10));
         }
         public ActionResult Detalhes(int? id)
