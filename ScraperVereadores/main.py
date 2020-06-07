943d3744305dd27387a73844e8e501e2551d90c6
lCamiloR <61637025+lCamiloR@users.noreply.github.com>
2020-06-07 12:39:52 -0300

    Update main.py

--- ScraperVereadores/main.py
+++ ScraperVereadores/main.py
@@ -1,12 +1,22 @@
-import funcoesScraping
-import funcoesBanco
+from tabula import read_pdf
+import pandas as pd
 
+PdfFile = read_pdf("http://www.cmmc.com.br/siteadmin/downloads/arquivos/04.2020.pdf", pages='all', multiple_tables=True)
 
-pointerScraper = funcoesScraping.Scraper('http://www.cmmc.com.br/vereadores/')
-finalLink = pointerScraper.getlinks()
-linkCompleto = []
+# for i in PdfFile:
+#     for index, row in i.iterrows():
+#         print(index, row)
+# aux = 1
+# tab1 = []
+# idParlamentar = 0
+tabela = PdfFile[0]
+for i in range(0,1):
+    print(tabela.iloc[i,1])
+    print(type(tabela.iloc[i, 1]))
+    print(tabela.iloc[i,1].split(maxsplit=1))
 
-for chunk in finalLink:
-    linkCompleto.append('http://www.cmmc.com.br/vereadores/' + chunk)
-for i in linkCompleto:
-    print(i)
+# colunas = PdfFile[1].columns
+# print(colunas[0])
+# print(type(colunas))
+
+# tab1.append(idParlamentar, nome, idCargo, cargo, vencimentoBase, outrosVencimentos, previdencia, irrf, outrosDescontos, liquido)
