cbf32a3ca9a8ef6bdb8cd70f95939d911b0e867a
Rai Benjamin da Silva <rai.benjamindasilva@gmail.com>
2020-06-07 17:31:09 -0300

    Adicionado Filtro para pagina projetos de lei

--- Site Se Liga Mogi/se_liga_mogi - 1.0/se_liga_mogi/Controllers/projetos_de_leiController.cs
+++ Site Se Liga Mogi/se_liga_mogi - 1.0/se_liga_mogi/Controllers/projetos_de_leiController.cs
@@ -15,7 +15,10 @@ namespace se_liga_mogi.Controllers
     {
         private Se_Liga_MogiEntities1 db = new Se_Liga_MogiEntities1();
 
-        public ActionResult Index(int pagina = 1, string Pesquisa = "")
+        public ActionResult Index(int pagina = 1, 
+            string Pesquisa = "", 
+            string AnoFiltro = ""
+            )
         {
             var q = db.projetos_de_lei.AsQueryable();
             if (!string.IsNullOrEmpty(Pesquisa))
@@ -24,6 +27,25 @@ namespace se_liga_mogi.Controllers
                 q = q.Where(c => c.numero_projeto.Contains(Pesquisa));
             }
             q = q.OrderBy(c => c.autor_projeto);
+
+            if (!string.IsNullOrEmpty(AnoFiltro)) {
+                //q = q.Where(c => c.ano_projeto = int.Parse(AnoFiltro));
+            }       
+
+            int AnoAtual = DateTime.Now.Year;
+
+            AnoAtual = AnoAtual - 2005; 
+
+            List<SelectListItem> Anos = new List<SelectListItem>();
+
+            for (int i = 0; i <= AnoAtual; i++) {
+                string valor = (i + 2005).ToString();
+                Anos.Add(new SelectListItem() { Text = valor, Value =valor });
+            }
+
+            SelectList AnosSelecao = new SelectList(Anos, "Text", "Value");
+
+            ViewBag.Anos = AnosSelecao;
             ViewBag.CurrentSort = Pesquisa;
             return View(q.ToPagedList(pagina, 10));
         }
