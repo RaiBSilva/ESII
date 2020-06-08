7134fc5b4d548b726d2131b59e271ca3ac8f4991
lCamiloR <61637025+lCamiloR@users.noreply.github.com>
2020-06-08 13:06:27 -0300

    pega todos os registros de um pdf.

--- ScraperVereadores/main.py
+++ ScraperVereadores/main.py
@@ -1,22 +1,20 @@
+
 from tabula import read_pdf
-import pandas as pd
 
 PdfFile = read_pdf("http://www.cmmc.com.br/siteadmin/downloads/arquivos/04.2020.pdf", pages='all', multiple_tables=True)
 
-# for i in PdfFile:
-#     for index, row in i.iterrows():
-#         print(index, row)
-# aux = 1
-# tab1 = []
-# idParlamentar = 0
-tabela = PdfFile[0]
-for i in range(0,1):
-    print(tabela.iloc[i,1])
-    print(type(tabela.iloc[i, 1]))
-    print(tabela.iloc[i,1].split(maxsplit=1))
+funcionarios = []
+# for i in range(0, len(PdfFile[0])):
+#     nomeId = PdfFile[0].iloc[i, 0].split(maxsplit=1)
+#     cargoId = PdfFile[0].iloc[i, 1].split(maxsplit=1)
+#     funcionarios.append((nomeId[0], nomeId[1], cargoId[0], cargoId[1], PdfFile[0].iloc[i, 2], PdfFile[0].iloc[i, 3], PdfFile[0].iloc[i, 4], PdfFile[0].iloc[i, 5], PdfFile[0].iloc[i, 6], PdfFile[0].iloc[i, 7]))
 
-# colunas = PdfFile[1].columns
-# print(colunas[0])
-# print(type(colunas))
+for i in range(1,len(PdfFile)):
+    colunas = PdfFile[i].columns
+    funcionarios.append((colunas[0], colunas[1], colunas[2], colunas[3], colunas[4], colunas[5], colunas[6], colunas[7], colunas[8], colunas[9]))
+    for row in range(0, len(PdfFile[i])):
+        tupla = PdfFile[i].iloc[row]
+        funcionarios.append((tupla[0], tupla[1], tupla[2], tupla[3], tupla[4], tupla[5], tupla[6], tupla[7], tupla[8], tupla[9]))
 
-# tab1.append(idParlamentar, nome, idCargo, cargo, vencimentoBase, outrosVencimentos, previdencia, irrf, outrosDescontos, liquido)
+for i in funcionarios:
+    print(i)
