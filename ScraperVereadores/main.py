40183ae56d2681f624312613054244824ea8d56b
lCamiloR <61637025+lCamiloR@users.noreply.github.com>
2020-07-12 20:56:57 -0300

    atualização da main

--- ScraperVereadores/main.py
+++ ScraperVereadores/main.py
@@ -1,17 +1,11 @@
-import Pdf04_2020
-import Pdf03_2020
 import Pdf02_2020
+import funcoesBD
 
+objDB = funcoesBD.dataBase("DSN=SQLEXPRESS;Server=localhost\sqlexpress;Database=Se_Liga_Mogi;Trusted_connection=yes;")
 
-funcionarios04_2020 = Pdf04_2020.LerPdf04()
-# funcionarios03_2020 = Pdf03_2020.LerPdf03()
-# funcionarios02_2020 = Pdf02_2020.LerPdf02()
-# for i in funcionarios02_2020:
-#     print(i)
-# for i in funcionarios03_2020:
-#     print(i)
-for i in funcionarios04_2020:
-    print(i)
+funcionarios02_2020 = Pdf02_2020.LerPdf02()
+funcionario_refatorados = []
+for i in funcionarios02_2020:
+    funcionario_refatorados.append((i[0],i[1],i[2],0))
 
-
-
+objDB.insertManyInTB(funcionario_refatorados,'INSERT INTO parlamentares(id_parlamentar, nome_parlamentar, cargo_parlamentar, total_faltas) VALUES (?, ?, ?, ?);')
