a2e2cab1f4ae6e7b47cda25ed1daa060ddeffdd8
lCamiloR <61637025+lCamiloR@users.noreply.github.com>
2020-07-12 20:58:21 -0300

    mudança do link

--- ScraperVereadores/Pdf02_2020.py
+++ ScraperVereadores/Pdf02_2020.py
@@ -2,7 +2,7 @@ import dadosPDF as dpdf
 import requests
 
 def LerPdf02():
-    url = 'http://www.cmmc.com.br/siteadmin/downloads/arquivos/02.2020.pdf'
+    url = 'http://www.cmmc.com.br/siteadmin/downloads/arquivos/05.2020.pdf'
     arquivoPDF = requests.get(url)
     listaDePaginas = dpdf.lerPDF(arquivoPDF)
     listaDeFuncionarios = dpdf.filtraDados02_2020(listaDePaginas)
